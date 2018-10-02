7f365eca858109dab189c2b74c95d0bddaff7acc
andrevaladares <andre.valadares@gmail.com>
2018-10-02 15:39:51 -0300

    Avançando com o projeto

--- src/chuck.groovy
+++ src/chuck.groovy
@@ -1,5 +1,16 @@
+import groovyx.net.http.ContentType
+import groovyx.net.http.RESTClient
 
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1' )
 
 String base = 'http://api.icndb.com'
 
+def chuck = new RESTClient(base)
+chuck.contentType = ContentType.JSON
+
+def params = [firstName: "André", lastName: "Valada"]
+
+chuck.get(path: '/jokes/random', query: params){ response, json ->
+    println response.status
+    println json
+}
